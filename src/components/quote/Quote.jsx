d5b3e81fd04fda67f1ed35e1600e23582fdb1de9
Elise Muellerleile <eliamue@live.com>
2021-09-15 12:55:46 -0500

    Quotejsx added

---
+++ src/components/quote/Quote.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Quote = ({ quote, character, image }) => (
+  <figure>
+    <img src={image} alt={character} />
+    <figcaption>
+      &quot;{quote}&quot; - {character}
+    </figcaption>
+  </figure>
+);
+
+Quote.propTypes = {
+  quote: PropTypes.string.isRequired,
+  character: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
