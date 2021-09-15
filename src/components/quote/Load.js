033a1c26c362db34a9647f40cd65145e94e642d2
Elise Muellerleile <eliamue@live.com>
2021-09-15 12:52:18 -0500

    load button function created

---
+++ src/components/quote/Load.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Load = ({ onClick }) => {
+  <button onClick={onClick} aria-label="Fetch!">
+    Load Simpsons Quote
+  </button>;
+};
+
+Load.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Load;
